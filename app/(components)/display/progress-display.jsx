8d018891e9a88d657d7b04a870d636c65b18beb8
Simone Montalti <simone.montalti@lutech.it>
2026-07-06 17:52:35 +0200

    removed placeholder

--- app/(components)/display/progress-display.jsx
+++ app/(components)/display/progress-display.jsx
@@ -1,9 +1,9 @@
-const ProgressDisplay = () => {
+const ProgressDisplay = ({ progress }) => {
   return (
     <div className='w-full bg-gray-500 rounded-full h-2.5'>
       <div
         className='bg-blue-500 h-2.5 rounded-full'
-        style={{ width: "75%" }}
+        style={{ width: `${progress}%` }}
       ></div>
     </div>
   );
