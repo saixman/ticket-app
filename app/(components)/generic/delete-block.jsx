bbcd37cfe91e7e4da6f187a2750fb7769d503030
Simone Montalti <simone.montalti@lutech.it>
2026-07-06 17:51:52 +0200

    dev delete component

--- app/(components)/generic/delete-block.jsx
+++ app/(components)/generic/delete-block.jsx
@@ -1,10 +1,31 @@
-import { faX } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+"use client";
 
-const DeleteBlock = () => {
-    return (
-        <FontAwesomeIcon icon={faX} className="text-red-500 hover:text-red-950"></FontAwesomeIcon>
-    )
-}
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
+
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+
+  const deleteTicket = async () => {
+    const res = await fetch(`/api/tickets/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to delete ticket.");
+    }
+
+    router.refresh();
+  };
+
+  return (
+    <FontAwesomeIcon
+      icon={faX}
+      className='text-red-500 hover:text-red-950'
+      onClick={deleteTicket}
+    ></FontAwesomeIcon>
+  );
+};
 
 export default DeleteBlock;
