ee3f428294bcb25d15ad81728eb80d49169d120f
Simone Montalti <simone.montalti@lutech.it>
2026-07-06 17:52:17 +0200

    dev priority component

--- app/(components)/display/priority-display.jsx
+++ app/(components)/display/priority-display.jsx
@@ -1,15 +1,30 @@
+import { MAX_FIRE_ICONS, TicketPriority } from "@/app/(models)/ticket-priority";
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const PriorityDisplay = () => {
-    return (
-        <div>
-            <FontAwesomeIcon icon={faFire} className="text-red-500"/>
-            <FontAwesomeIcon icon={faFire} className="text-red-500" />
-            <FontAwesomeIcon icon={faFire} className="text-red-500"/>
-            <FontAwesomeIcon icon={faFire} className="text-red-500"/>
-            </div>
-    )
-}
+const PriorityDisplay = ({ priority }) => {
+  const colorMap = {
+    [TicketPriority.HIGH]: "text-red-500",
+    [TicketPriority.MEDIUM]: "text-yellow-500",
+    [TicketPriority.LOW]: "text-gray-500",
+  };
+
+  let getColor = (level) => {
+    const actualPriority = Math.max(priority, level);
+    return colorMap[actualPriority];
+  };
+
+  return (
+    <div className='flex justify-start align-baseline'>
+      {Array.from({ length: MAX_FIRE_ICONS }).map((_, index) => (
+        <FontAwesomeIcon
+          key={index}
+          icon={faFire}
+          className={getColor(index + 1)}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default PriorityDisplay;
