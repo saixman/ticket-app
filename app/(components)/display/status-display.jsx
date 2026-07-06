3e1fae215df6819a5003e63003f4949b609e392b
Simone Montalti <simone.montalti@lutech.it>
2026-07-06 17:52:54 +0200

    dev status chip component

--- app/(components)/display/status-display.jsx
+++ app/(components)/display/status-display.jsx
@@ -1,9 +1,21 @@
-const StatusDisplay = () => {
-    return (
-        <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-500">
-            Done!!
-        </span>
-    )
-}
+import { TicketStatus } from "@/app/(models)/ticket-status";
+
+const StatusDisplay = ({ status }) => {
+  const colorMap = {
+    [TicketStatus.DONE]: "bg-green-500",
+    [TicketStatus.STARTED]: "bg-yellow-500",
+    [TicketStatus.NOT_STARTED]: "bg-red-500",
+  };
+
+  const getColor = () => colorMap[status.toLowerCase()];
+
+  return (
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-amber-100 ${getColor()}`}
+    >
+      {status}
+    </span>
+  );
+};
 
 export default StatusDisplay;
