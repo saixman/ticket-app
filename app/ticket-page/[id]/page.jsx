2d4e2a2076caadf997211dbb397a169daf1a8d65
Simone Montalti <simone.montalti@lutech.it>
2026-07-06 17:53:38 +0200

    dev ticket edit page

--- app/ticket-page/[id]/page.jsx
+++ app/ticket-page/[id]/page.jsx
@@ -1,5 +1,45 @@
-const TicketPage = ({ params }) => {
-    return <div>TicketPage { params.id }</div>
-}
+import TicketForm from "@/app/(components)/data-handling/edit-ticket-form";
+
+const PageViewType = {
+  CREATE: "CREATE",
+  EDIT: "EDIT",
+};
+
+const getTicketById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+      method: "GET",
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to get ticket");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("Failed");
+  }
+};
+
+const TicketPage = async ({ params }) => {
+  const { id } = await params;
+
+  const pageView = id == "new" ? PageViewType.CREATE : PageViewType.EDIT;
+  let updateTicket = {};
+
+  const isEditMode = pageView == PageViewType.EDIT;
+
+  if (isEditMode) {
+    const { content } = await getTicketById(id);
+    updateTicket = content;
+  } else {
+    updateTicket = {
+      _id: "new",
+    };
+  }
+
+  return <TicketForm ticket={updateTicket} isEditMode={isEditMode} />;
+};
 
 export default TicketPage;
