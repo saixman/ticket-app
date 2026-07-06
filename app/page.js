9f19ee61b5b720145e3cd28a846e3a7c77a60048
Simone Montalti <simone.montalti@lutech.it>
2026-07-06 17:51:39 +0200

    dev dashboard - home page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,46 @@
 import React from "react";
-import TicketCard from "./(components)/ticket-card";
+import TicketCard from "./(components)/display/ticket-card";
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/tickets", {
+      cache: "no-store",
+    });
+
+    return res.json();
+  } catch (error) {
+    console.error("GET Tickets failed");
+  }
+};
+
+const Dashboard = async () => {
+  const { content: tickets } = await getTickets();
+
+  const uniqueCategories = [
+    ...new Set(tickets.map(({ category }) => category)),
+  ];
+
   return (
-    <div>
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
+    <div className='p-5'>
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, index) => (
+            <div key={index} className='mb-4'>
+              <h2>{uniqueCategory}</h2>
+              <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
